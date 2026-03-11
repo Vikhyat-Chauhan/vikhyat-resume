340ba08c7b511694eba75498e82ba649f5473f62
Vikhyat Chauhan <vikhyat.chauhan@gmail.com>
2026-03-11 01:37:28 -0400

    Tried to fix deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,21 @@
 import react from '@astrojs/react'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import expressiveCode from 'astro-expressive-code'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-import rehypeExternalLinks from 'rehype-external-links'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import { expressiveCodeOptions } from './src/site.config'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.me',
 	integrations: [
-		expressiveCode(expressiveCodeOptions),
 		tailwind({
 			applyBaseStyles: false
 		}),
 		sitemap(),
-		mdx(),
 		icon(),
 		react()
 	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	// build: {
-	// 	outDir: './dist',
-	// },
 	prefetch: true,
 	output: 'static'
 })
